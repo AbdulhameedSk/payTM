780a54f96d82a35128ac06acd31fb5dbf61c65b6
SHAIK ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-03-18 11:31:36 +0530

    Testing APIs

--- Backend/index.js
+++ Backend/index.js
@@ -7,4 +7,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
-app.listen(3000)
+app.listen(3000, () => {
+ console.log('Server is running at http://localhost:3000');
+});
