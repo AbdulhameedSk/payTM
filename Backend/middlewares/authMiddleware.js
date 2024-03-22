5ae3236abfbe304807d8f1fedd11c5ba319995e4
SHAIK ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-03-22 15:13:59 +0530

    COMPLETED BACKEND

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
+console.log(authHeader);
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
       return res.status(403).json({});
   }
--- Backend/routes/account.js
+++ Backend/routes/account.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { balance, transfer } = require("../controllers/accountController");
 const router = express.Router();
-
+const { authMiddleware } = require("../middlewares/authMiddleware");
+router.use(authMiddleware);
 router.get("/balance", balance);
 router.post("/transfer", transfer);
 
