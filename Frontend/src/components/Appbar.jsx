0996d76eb38dd70040ef27231c0406285e1a2e33
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-03-30 16:45:35 +0530

    Completed components

--- Frontend/src/components/Appbar.jsx
+++ Frontend/src/components/Appbar.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-
+import React from "react";
 export const Appbar = () => {
   return (
-    <div>Appbar</div>
-  )
-}
+    <div className="border flex justify-between">
+      <div className="flex items-center p-3">PayTM</div>
+      <div className="flex items-center">
+        Hello
+        <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 m-2">
+          <svg
+            className="absolute w-12 h-12 text-gray-400 -left-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
--- Frontend/src/components/Users.jsx
+++ Frontend/src/components/Users.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
 export const Users = () => {
+  const [users, setUsers] = useState([
+    { firstName: "SHAIK", lastName: "ABDULHAMEED", _id: 1 },
+  ]);
   return (
-    <div>Users</div>
-  )
+    <div className="m-5">
+      <div className="font-bold">Users</div>
+      <div className="flex mt-2">
+        <input
+          className="border rounded border-black w-full placeholder-py-2 placeholder-px-4 p-2 "
+          type="text"
+          placeholder="select users"
+        />
+        {/* <input
+          className="bg-black text-white px-4 py-2 border border-white rounded"
+          type="button"
+          value="Search"
+        />{" "} */}
+      </div>
+      <div>
+        {users.map((user) => (
+          <User user={user} />
+        ))}{" "}
+      </div>
+    </div>
+  );
+};
+
+function User({ user }) {
+  return (
+    <div className="flex justify-between my-2 border-b">
+      <div>
+        {user.firstName} {user.lastName}
+      </div>
+      <div>
+        <input
+          className="bg-black text-white px-4 py-2 border border-white rounded"
+          type="button"
+          value="Send Money"
+        />
+      </div>
+    </div>
+  );
 }
